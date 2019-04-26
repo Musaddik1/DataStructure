3f1cdcfb83fddedf9037270a15b9ad27eced0e1c
Musaddik1 <49139922+Musaddik1@users.noreply.github.com>
2019-04-26 11:44:54 +0530

    Update Dequeue.java

--- Dequeue.java
+++ Dequeue.java
@@ -1,103 +1,109 @@
 package com.DataStructure;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Dequeue {
-	char array[]=new char[10];
-	int rear=0;
-	int front=0;
-	public void Insert_back(char no)
-	{
-		if(IsFull())
-		{
+	char array[] = new char[10];
+	int rear = 0;
+	int front = 0;
+
+	public void Insert_back(char no) {
+		if (IsFull()) {
 			System.out.println("Overflow");
-		}
-		else
-		{
-			array[rear]=no;
+		} else {
+			array[rear] = no;
 			rear++;
 		}
 	}
-	public void Insert_front(char no)
-	{
-		if(IsFull())
-		{
+
+	public void Insert_front(char no) {
+		if (IsFull()) {
 			System.out.println("Overflow");
-		}
-		else
-		{
-			for(int i=rear;i>front;i--)
-			{
-				array[i]=array[i-1];
+		} else {
+			for (int i = rear; i > front; i--) {
+				array[i] = array[i - 1];
 			}
-			array[front]=no;
+			array[front] = no;
 			rear++;
-			
+
 		}
-		
+
 	}
-	public char Delete_back()
-	{
-		if(IsEmpty())
-		{
+
+	public char Delete_back() {
+		if (IsEmpty()) {
 			System.out.println("Underflow");
 			return ' ';
-		}
-		else
-		{
+		} else {
 			rear--;
-			char ch=array[rear];
-			
+			char ch = array[rear];
+
 			return ch;
 		}
 	}
-	public char Delete_front()
-	{
-		if(IsEmpty())
-		{
+
+	public char Delete_front() {
+		if (IsEmpty()) {
 			System.out.println("Underflow");
 			return ' ';
-		}
-		else
-		{
-			 char ch=array[front];
+		} else {
+			char ch = array[front];
 			front++;
-			
+
 			return ch;
 		}
 	}
-	public void display()
-	{
-		for(int i=front;i<rear;i++)
-		{
+
+	public void display() {
+		for (int i = front; i < rear; i++) {
 			System.out.println(array[i]);
 		}
 	}
-	public int Size()
-	{
-		int count=0;
-		for(int i=front;i<rear;i++)
-		{
+
+	public int Size() {
+		int count = 0;
+		for (int i = front; i < rear; i++) {
 			count++;
 		}
 		return count;
 	}
-	public boolean IsEmpty()
-	{
-		if(front==rear)
-		{
-		 return true;
-		 
-		}
-		else
-		{
+
+	public boolean IsEmpty() {
+		if (front == rear) {
+			return true;
+
+		} else {
 			return false;
 		}
 	}
-	public boolean IsFull()
+
+	public boolean IsFull() {
+		if (rear == array.length) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public boolean Palindrome()
 	{
-		if(rear==array.length)
+		boolean result = false;
+		while(front!=rear)
+		{
+			
+			if(array[front]==array[rear])
+			{
+				front++;
+				rear++;
+				 result=true;
+			}
+			else
+			{
+				break;
+			}
+		}
+		if(result==true)
 		{
 			return true;
 		}
@@ -105,35 +111,37 @@ public class Dequeue {
 		{
 			return false;
 		}
-	}
-	public static void main(String[] args) {
+			
 		
-		Dequeue dequeue=new Dequeue();
-		Scanner scanner=new Scanner(System.in);
+	}
+
+	public static void main(String[] args) {
+
+		Dequeue dequeue = new Dequeue();
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter String :");
-		String string=scanner.next();
-		char ch[]=string.toCharArray();
-		for(int i=0;i<ch.length;i++)
-		{
+		String string = scanner.next();
+		char ch[] = string.toCharArray();
+		for (int i = 0; i < ch.length; i++) {
 			dequeue.Insert_front(ch[i]);
 		}
-	
-		char ch1[]=new char[ch.length];
-		char ch2[]=new char[ch.length];
-		int len=string.length()-1;
-		int count=0;
-		
-		String string1=new String(ch1);
-		for(char var:ch1)
-		{
+
+		char ch1[] = new char[ch.length];
+		char ch2[] = new char[ch.length];
+		int len = string.length() - 1;
+		int count = 0;
+
+		String string1 = new String(ch1);
+		for (char var : ch1) {
 			System.out.println(var);
 		}
-	
-		
-		/*
-		 * if(string1.equals(string)) { System.out.println("Panlindrome"); } else {
-		 * System.out.println("Not Panlindrome"); }
-		 */
+
+		if (dequeue.Palindrome()) {
+			System.out.println("Panlindrome");
+		} else {
+
+			System.out.println("Not Panlindrome");
+		}
 	}
 
 }
